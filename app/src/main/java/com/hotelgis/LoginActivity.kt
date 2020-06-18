67fff2dcef55834f4a008fa2c686e94427856ece
regawarp <regawa04@gmail.com>
2020-06-18 11:17:04 +0700

    Finishing layout login; Change activity dari login ke main ketika klik tombol Login Admin

--- app/src/main/java/com/hotelgis/LoginActivity.kt
+++ app/src/main/java/com/hotelgis/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.hotelgis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLoginAdmin = findViewById(R.id.btn_login_admin) as Button
+        btnLoginAdmin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+            finish()
+        }
     }
 }
