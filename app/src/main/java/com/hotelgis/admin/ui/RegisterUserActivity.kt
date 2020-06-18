8820687b93f7c577558c8d78475b9ba61c25344d
mumuhkustino <mumuhkustino@gmail.com>
2020-06-18 10:45:17 +0700

    Update register user activity.

--- app/src/main/java/com/hotelgis/admin/ui/RegisterUserActivity.kt
+++ app/src/main/java/com/hotelgis/admin/ui/RegisterUserActivity.kt
@@ -1,13 +1,27 @@
 package com.hotelgis.admin.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.hotelgis.R
+import kotlinx.android.synthetic.main.activity_register_user.*
 
 class RegisterUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_user)
+
+        toolbar.title = "Register User"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
