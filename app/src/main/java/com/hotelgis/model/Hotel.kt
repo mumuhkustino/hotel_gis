75f41bc3368c82975d4fafad9de623769f977c7d
regawarp <regawa04@gmail.com>
2020-06-22 06:58:44 +0700

    Get and Show data hotel di firestore untuk ditampilkan di User(MapsActivity)

--- app/src/main/java/com/hotelgis/model/Hotel.kt
+++ app/src/main/java/com/hotelgis/model/Hotel.kt
@@ -5,11 +5,11 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Hotel(
-    var name: String,
-    var address: String,
-    var phone: String,
-    var image: String,
-    var lat: String,
-    var long: String,
-    var rooms: ArrayList<Room>
+    var name: String = "",
+    var address: String = "",
+    var phone: String = "",
+    var image: String = "",
+    var lat: String = "",
+    var long: String = "",
+    var rooms: ArrayList<Room> = ArrayList()
 ) : Parcelable
